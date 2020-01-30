261dde2366b7083946357f8e32d11ac84cb45ff0
Pozas91 <pozas91@gmail.com>
2020-01-30 15:30:25 +0100

    The VueJS Instance Lifecycle

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,35 @@
-let data = {
-    title: 'The VueJS Instance',
-    showParagraph: false
-};
-
-Vue.component('hello', {
-    template: '<h1>Hello!</h1>'
-});
-
-let vm1 = new Vue({
-    el: '#app1',
-    data: data,
+new Vue({
+    el: '#app',
+    data: {
+        title: 'The VueJS Instance'
+    },
+    beforeCreate: function () {
+        console.log('beforeCreate()');
+    },
+    created: function () {
+        console.log('created()');
+    },
+    beforeMount: function () {
+        console.log('beforeMount()');
+    },
+    mounted: function () {
+        console.log('mounted()');
+    },
+    beforeUpdate: function () {
+        console.log('beforeUpdate()');
+    },
+    updated: function () {
+        console.log('updated()');
+    },
+    beforeDestroy: function () {
+        console.log('beforeDestroy()');
+    },
+    destroyed: function () {
+        console.log('destroyed()');
+    },
     methods: {
-        show: function () {
-            this.showParagraph = true;
-            this.updateTitle('The VueJS Instance (Updated)');
-            this.$refs.myButton.innerText = 'Test';
-            console.log(this.$refs.myButton);
-        },
-        updateTitle: function (title) {
-            this.title = title;
-        }
-    },
-    computed: {
-        lowercaseTitle: function () {
-            return this.title.toLowerCase();
-        }
-    },
-    watch: {
-        title: function (value) {
-            alert('Title changed, new value: ' + value);
+        destroy: function () {
+            this.$destroy();
         }
     }
 });
-
-vm1.$mount('#app1');
-
-vm1.newPro = 'New!';
-console.log(vm1.$data === data);
-vm1.$refs.heading.innerText = 'Something else';
-
-setTimeout(function () {
-    vm1.title = 'Changed by Timer';
-    vm1.show();
-}, 3000);
-
-let vm2 = new Vue({
-    el: '#app2',
-    data: {
-        title: 'The second Instance',
-    },
-    methods: {
-        onChange: function () {
-            vm1.title = 'Changed!';
-        }
-    },
-});
-
-let vm3 = new Vue({
-    el: 'hello',
-    template: '<h1>Hello!</h1>'
-});
-
-// vm3.$mount('#app3');
-// vm3.$mount();
-// document.getElementById('app3').appendChild(vm3.$el);
