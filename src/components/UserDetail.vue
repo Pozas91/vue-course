c193b2919a827b5c98f295f09b8ab42024e8d1da
Pozas91 <pozas91@gmail.com>
2020-02-04 15:24:50 +0100

    Communicating with Callback Functions

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -4,6 +4,7 @@
     <p>Many Details</p>
     <p>User Name: {{switchName()}}</p>
     <button @click="resetName">Reset Name</button>
+    <button @click="resetFn()">Reset Name</button>
   </div>
 </template>
 
@@ -20,7 +21,8 @@
         //     name: 'Max'
         //   }
         // }
-      }
+      },
+      resetFn: Function
     },
     methods: {
       switchName() {
