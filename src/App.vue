31d1aef1133102eb0b64cb15cbf72368cf387bad
Pozas91 <pozas91@gmail.com>
2020-02-05 15:25:22 +0100

    Wrap Up

--- src/App.vue
+++ src/App.vue
@@ -2,44 +2,46 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <button @click="selectedComponent = 'appQuote'">Quote</button>
-        <button @click="selectedComponent = 'appAuthor'">Author</button>
-        <button @click="selectedComponent = 'appNew'">New</button>
-        <hr/>
-        <p>{{selectedComponent}}</p>
-        <keep-alive>
-          <component :is="selectedComponent">
-            <p>Default content</p>
-          </component>
-        </keep-alive>
-        <!--        <app-quote>-->
-        <!--          <h2 slot="title">{{quoteTitle}}</h2>-->
-        <!--          <p>A wonderful Quote</p>-->
-        <!--        </app-quote>-->
+        <br>
+        <button class="btn btn-primary" @click="selectedComponent = 'appBlue'">Load Blue Template</button>
+        <button class="btn btn-success" @click="selectedComponent = 'appGreen'">Load Green Template</button>
+        <button class="btn btn-danger" @click="selectedComponent = 'appRed'">Load Red Template</button>
+        <hr>
+        <component :is="selectedComponent">
+          <p>This is the Content</p>
+        </component>
+        <!--<app-blue>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-blue>-->
+        <!--<app-green>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-green>-->
+        <!--<app-red>-->
+        <!--<p>This is the Content</p>-->
+        <!--</app-red>-->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Quote from "./components/Quote";
-  import Author from "./components/Author";
-  import New from "./components/New";
+  import Blue from './components/Blue.vue';
+  import Green from './components/Green.vue';
+  import Red from './components/Red.vue';
 
   export default {
     data: function () {
       return {
-        quoteTitle: 'The Quote',
-        selectedComponent: 'appQuote'
+        selectedComponent: 'appBlue'
       }
     },
     components: {
-      appQuote: Quote,
-      appAuthor: Author,
-      appNew: New,
+      appBlue: Blue,
+      appGreen: Green,
+      appRed: Red
     }
   }
 </script>
 
-<style scoped>
+<style>
 </style>
