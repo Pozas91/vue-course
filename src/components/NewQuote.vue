f412818bd655ba9cf141774e63c2c1344ac73b78
Pozas91 <pozas91@gmail.com>
2020-02-06 15:02:52 +0100

    Adding Quotes with Custom Events

--- src/components/NewQuote.vue
+++ src/components/NewQuote.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="row">
-    <form>
-      <div class="col-sm-8 col-sm-offset-2 col-12 col-md-6 col-md-offset-3 form-group">
-        <label>Quote</label>
-        <textarea class="form-control" rows="3" v-model="quote"></textarea>
-      </div>
-      <div class="col-sm-8 col-sm-offset-2 col-12 col-md-6 col-md-offset-3 form-group">
-        <button class="btn btn-primary" @click.prevent="createNew">
-          Add Quote
-        </button>
+    <form class="col">
+      <div class="row">
+        <div class="col-sm-8 col-sm-offset-2 col-12 col-md-6 col-md-offset-3 form-group">
+          <label>Quote</label>
+          <textarea class="form-control" rows="3" v-model="quote"></textarea>
+        </div>
+        <div class="col-sm-8 col-sm-offset-2 col-12 col-md-6 col-md-offset-3 form-group">
+          <button class="btn btn-primary" @click.prevent="createNew">
+            Add Quote
+          </button>
+        </div>
       </div>
     </form>
   </div>
@@ -23,7 +25,8 @@
     },
     methods: {
       createNew() {
-
+        this.$emit('quoteAdded', this.quote);
+        this.quote = '';
       }
     }
   }
