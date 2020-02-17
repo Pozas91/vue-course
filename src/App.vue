7e03d60f70aeb9e4cce6e38fbd5ac4cea588372c
Pozas91 <pozas91@gmail.com>
2020-02-17 20:10:16 +0100

    Creating a Local Filter

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,26 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1>Section Title</h1>
-
+        <h1>Filters & Mixins</h1>
+        <p>{{text | toUppercase }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        text: 'Hello there!'
+      }
+    },
+    filters: {
+      toUppercase(value) {
+        return value.toUpperCase();
+      }
+    }
+  }
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// Global Filter
+Vue.filter('to-lowercase');
+
 new Vue({
   el: '#app',
   render: h => h(App)
