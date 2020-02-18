af044e83bac561fcd5115ef7d832608d729193a8
Pozas91 <pozas91@gmail.com>
2020-02-18 20:24:15 +0100

    Preparing Code to use Transitions

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,26 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h1>Section Title</h1>
-
+        <h1>Animations and Transitions</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br/> <br/>
+        <transition>
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
 </script>
 
 <style>
