0d635a92172a70be86e41086d82898dc687e7b42
Pozas91 <pozas91@gmail.com>
2020-02-25 13:20:30 +0100

    Intercepting Responses

--- src/main.js
+++ src/main.js
@@ -12,7 +12,13 @@ Vue.http.interceptors.push((request, next) => {
     request.method = 'PUT';
   }
 
-  next();
+  next(response => {
+    response.json = () => {
+      return {
+        messages: response.body
+      }
+    }
+  });
 });
 
 new Vue({
