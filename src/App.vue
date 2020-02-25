71d8f39213f30920a0b503fe5d7cd44ff64084d1
Pozas91 <pozas91@gmail.com>
2020-02-25 13:41:32 +0100

    Resources vs "Normal" Http Requests Understanding Template URLs Wrap Up

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
         <hr/>
+        <input class="form-control" type="text" v-model="node"/>
+        <br/><br/>
         <button class="btn btn-primary" @click="fetchData">Get Data</button>
         <ul class="list-group">
           <li class="list-group-item" v-for="u in users">{{u.username}} - {{u.email}}</li>
@@ -31,7 +33,8 @@
           email: ''
         },
         users: [],
-        resource: {}
+        resource: {},
+        node: 'data'
       };
     },
     methods: {
@@ -47,8 +50,22 @@
         this.resource.saveAlt(this.user);
       },
       fetchData() {
-        this.$http
-          .get('data.json')
+        // this.$http
+        //   .get('data.json')
+        //   .then(response => {
+        //     const data = response.json();
+        //     return data;
+        //   })
+        //   .then(data => {
+        //     const resultArray = [];
+        //
+        //     for (let key in data) {
+        //       resultArray.push(data[key])
+        //     }
+        //
+        //     this.users = resultArray;
+        //   });
+        this.resource.getData({node: this.node})
           .then(response => {
             const data = response.json();
             return data;
@@ -66,10 +83,10 @@
     },
     created() {
       const customActions = {
-        saveAlt: {method: 'POST', url: 'alternative.json'}
+        saveAlt: {method: 'POST', url: 'alternative.json'},
+        getData: {method: 'GET'}
       };
-
-      this.resource = this.$resource('data.json', {}, customActions);
+      this.resource = this.$resource('{node}.json', {}, customActions);
     }
   }
 </script>
