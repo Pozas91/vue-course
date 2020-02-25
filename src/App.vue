6d59741a08ac4cb1a189e50c1cb018ce5a20075b
Pozas91 <pozas91@gmail.com>
2020-02-25 12:43:25 +0100

    Creating an Application and Setting Up a Server (Firebase)

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,36 @@
     <div class="row">
       <div class="col-12">
         <h1>Http</h1>
-
+        <div class="form-group">
+          <label>Username</label>
+          <input class="form-control" type="text" v-model="user.username"/>
+        </div>
+        <div class="form-group">
+          <label>Mail</label>
+          <input type="text" class="form-control" v-model="user.email"/>
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          email: ''
+        }
+      };
+    },
+    methods: {
+      submit() {
+        console.log(this.user);
+      }
+    }
+  }
 </script>
 
 <style>
