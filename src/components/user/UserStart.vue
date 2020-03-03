946b03b7ba077fcd050c0564fe61f75db36368b3
Pozas91 <pozas91@gmail.com>
2020-03-03 19:22:20 +0100

    Setting Up Child Routes (Nested Routes)

--- src/components/user/UserStart.vue
+++ src/components/user/UserStart.vue
@@ -7,6 +7,7 @@
       <li class="list-group-item" style="cursor: pointer">User 2</li>
       <li class="list-group-item" style="cursor: pointer">User 3</li>
     </ul>
+  </div>
 </template>
 
 <script>
