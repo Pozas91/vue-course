1cab6bb77d891050c23ed59ffae287381d9d053f
Pozas91 <pozas91@gmail.com>
2020-03-04 15:47:08 +0100

    Using the "beforeEnter" Guard

--- src/components/user/UserDetail.vue
+++ src/components/user/UserDetail.vue
@@ -27,6 +27,13 @@
           hash: '#data'
         }
       }
+    },
+    beforeRouteEnter(to, from, next) {
+      if (true) {
+        next();
+      } else {
+        next(false);
+      }
     }
   }
 </script>
