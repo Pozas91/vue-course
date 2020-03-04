532e753a6256aa5d1dc1cae4666012a89135b8e5
Pozas91 <pozas91@gmail.com>
2020-03-04 19:47:04 +0100

    Why Mutations have to run Synchronously How Actions improve Mutations Using Actions

--- src/store/store.js
+++ src/store/store.js
@@ -22,5 +22,23 @@ export const store = new Vuex.Store({
     decrement: state => {
       state.counter--;
     },
+  },
+  actions: {
+    increment: ({commit}) => {
+      commit('increment');
+    },
+    decrement: context => {
+      context.commit('decrement');
+    },
+    asyncIncrement: ({commit}) => {
+      setTimeout(() => {
+        commit('increment');
+      }, 1000);
+    },
+    asyncDecrement: ({commit}) => {
+      setTimeout(() => {
+        commit('decrement');
+      }, 1000);
+    }
   }
 });
