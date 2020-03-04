e36a3cd5cdf67eaa6dbb10b46f64fcac37a31eed
Pozas91 <pozas91@gmail.com>
2020-03-04 15:51:30 +0100

    Using the "beforeLeave" Guard

--- src/components/user/UserEdit.vue
+++ src/components/user/UserEdit.vue
@@ -4,7 +4,7 @@
     <p>Locale: {{$route.query.locale}}</p>
     <p>Analytics: {{$route.query.q}}</p>
     <hr/>
-    <button class="btn btn-primary">Confirm</button>
+    <button class="btn btn-primary" @click="confirmed = true">Confirm</button>
     <div style="height: 700px"></div>
     <p id="data">Some extra Data</p>
   </div>
@@ -12,7 +12,23 @@
 
 <script>
   export default {
-    name: "UserEdit"
+    data() {
+      return {
+        confirmed: false
+      }
+    },
+    beforeRouteLeave(to, from, next) {
+      if (this.confirmed) {
+        next();
+      } else {
+        if (confirm('Are you sure?')) {
+          next();
+        } else {
+          next(false);
+        }
+      }
+    },
+    methods: {}
   }
 </script>
 
