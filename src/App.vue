8b8bea3ee4f8fd91f47bc63281e51d59b83f68e6
Pozas91 <pozas91@gmail.com>
2020-03-04 17:41:36 +0100

    Why a Different State Management May Be Needed Understanding "Centralized State" Using the Centralized State

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-12">
-        <h1>Section Title</h1>
-
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result></app-result>
+        <hr>
+        <app-counter></app-counter>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  import Counter from './components/Counter.vue';
+  import Result from './components/Result.vue';
+
+  export default {
+    components: {
+      appCounter: Counter,
+      appResult: Result,
+    }
+  }
 </script>
 
-<style>
-</style>
