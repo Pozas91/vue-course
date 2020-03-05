0f977e5ef38da559a6cba20112bef4b4a3318fe8
Pozas91 <pozas91@gmail.com>
2020-03-05 23:31:48 +0100

    Project Introduction

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-12">
-        <h1>Section Title</h1>
+    <div class="container">
 
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {}
+
+    export default {
+
+    }
 </script>
 
 <style>
+
 </style>
