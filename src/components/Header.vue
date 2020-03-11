b1879d201579cb56dbc4050ed10e6b0c678209b7
Pozas91 <pozas91@gmail.com>
2020-03-11 19:45:08 +0100

    Adding a Header and Navigation

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,42 @@
 <template>
-  <h1>The Header Component</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <router-link to="/portfolio" activeClass="active" tag="li" class="nav-item">
+          <a class="nav-link">Portfolio</a>
+        </router-link>
+
+        <router-link to="/stocks" activeClass="active" tag="li" class="nav-item">
+          <a class="nav-link">Stocks</a>
+        </router-link>
+
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
+             aria-haspopup="true" aria-expanded="false">
+            Save Data
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#">Save Data</a>
+            <a class="dropdown-item" href="#">Load Data</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-    export default {
-        name: "Home"
-    }
+  export default {
+    name: "Home"
+  }
 </script>
 
 <style scoped>
