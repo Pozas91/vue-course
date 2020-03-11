edd6d24228508d317fcc74c9d2c629040aeb5acf
Pozas91 <pozas91@gmail.com>
2020-03-11 20:00:13 +0100

    Planning the Next Steps Creating Stocks Components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>The Stocks - Stock Component</h1>
+  <div class="col-6 col-md-4 mt-3">
+    <div class="card">
+
+      <div class="card-header">
+        <h5 class="card-title">
+          NAME
+        </h5>
+
+        <h6 class="card-subtitle mb-2 text-muted">Price: PRICE</h6>
+      </div>
+
+      <div class="card-body">
+        <input type="number" class="form-control" placeholder="Quantity"/>
+        <button class="btn btn-success">
+          Buy
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Stock"
-    }
 </script>
 
 <style scoped>
