232c9c39c83d1676f4722df1d2ef9d87915a62a1
Pozas91 <pozas91@gmail.com>
2020-03-27 10:24:32 +0100

    Ending the Day - Randomizing Stocks

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -20,6 +20,11 @@
       </ul>
 
       <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a href="#" @click="endDay" class="nav-link">
+            End Day
+          </a>
+        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown"
              aria-haspopup="true" aria-expanded="false">
@@ -38,10 +43,20 @@
 </template>
 
 <script>
+  import {mapActions} from 'vuex';
+
   export default {
     computed: {
       funds() {
         return this.$store.getters.funds;
+      }
+    },
+    methods: {
+      ...mapActions([
+        'randomizeStocks'
+      ]),
+      endDay() {
+        this.randomizeStocks();
       }
     }
   }
