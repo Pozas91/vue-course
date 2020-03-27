2fa75bcc279fd298c3224457a1218db5a008e091
Pozas91 <pozas91@gmail.com>
2020-03-27 19:45:21 +0100

    Using Interceptors

--- src/main.js
+++ src/main.js
@@ -9,6 +9,20 @@ axios.defaults.baseURL = 'https://curso-vue-cd4c5.firebaseio.com/';
 axios.defaults.headers.common['Authorization'] = 'fastas';
 axios.defaults.headers.get['Accepts'] = 'application/json';
 
+const reqInterceptor = axios.interceptors.request.use(config => {
+  console.log('Request Interceptor', config);
+  return config
+});
+
+const resInterceptor = axios.interceptors.response.use(res => {
+  console.log('Response Interceptor', res);
+  return res;
+});
+
+// Removing interceptors... if we want to apply upper interceptors comment below lines.
+axios.interceptors.request.eject(reqInterceptor);
+axios.interceptors.response.eject(resInterceptor);
+
 new Vue({
   el: '#app',
   router,
