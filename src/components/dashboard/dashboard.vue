6c3279c17d91f089f7ffc20b2bfadd8b11c44d12
Pozas91 <pozas91@gmail.com>
2020-03-27 19:24:27 +0100

    Sending a GET Request

--- src/components/dashboard/dashboard.vue
+++ src/components/dashboard/dashboard.vue
@@ -5,6 +5,19 @@
   </div>
 </template>
 
+<script>
+  import axios from 'axios';
+
+  export default {
+    created() {
+      axios
+        .get('https://curso-vue-cd4c5.firebaseio.com/users.json')
+        .then(res => console.log(res))
+        .catch(error => console.log(error));
+    }
+  }
+</script>
+
 <style scoped>
   h1, p {
     text-align: center;
