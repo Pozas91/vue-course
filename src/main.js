4bae6ec320e37212f6a75b19d39f3d008851f4bc
Pozas91 <pozas91@gmail.com>
2020-03-27 10:42:01 +0100

    Setting up vue-resource and Firebase

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
 import VueRouter from "vue-router";
+import VueResource from 'vue-resource';
+
 import {routes} from "./routes";
+import App from './App.vue'
 import store from './store/store';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://curso-vue-cd4c5.firebaseio.com/';
 
 Vue.filter('currency', (value) => {
   return '$' + value.toLocaleString();
