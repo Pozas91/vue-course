80997a7b72bb2af9620641a762abc6d86436e564
Pozas91 <pozas91@gmail.com>
2020-03-27 13:38:58 +0100

    Preparing for Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -105,7 +105,7 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        // warning: false
+        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
