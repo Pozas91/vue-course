3f305ed65a7213508ffe50624f6c4172d1189a0a
Pozas91 <pozas91@gmail.com>
2020-03-29 14:45:10 +0200

    Creating Project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
+  router,
+  store,
   render: h => h(App)
-});
+}).$mount('#app')
